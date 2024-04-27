b2ebd23f82acc043b76b80a37917d25e10727c55
hafsaabidsaeed <128362965+hafsaabidsaeed@users.noreply.github.com>
2024-04-27 23:43:12 +0500

    Java basics
    Input output in Java
    Variables in Java
    Mathematical operations in Java

---
+++ firstday/src/firstday.java
@@ -0,0 +1,26 @@
+public class firstday {
+    public static void main(String[] args) throws Exception {
+        // print, println, \n
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+        System.out.print("Hello, World!\n");
+
+        // Widenning concatenation
+        int num1 = 10;
+        double num2 = num1;
+        System.out.println(num1);
+        System.out.println(num2);
+
+        // Narrowing concatenation
+        double num3 = 10.4;
+        int num4 = (int) num3;
+        System.out.println(num3);
+        System.out.println(num4);
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+    }
+}
---
+++ javaanddsa/src/lecture2.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class lecture2 {
+    public static void main(String[] args) throws Exception {
+        // // print, println, \n
+        // System.out.println("Hello, World!");
+        // System.out.print("Hello, World!");
+        // System.out.print("Hello, World!\n");
+
+        // // Widenning concatenation
+        // int num1 = 10;
+        // double num2 = num1;
+        // System.out.println(num1);
+        // System.out.println(num2);
+        // // Narrowing concatenation
+        // double num3 = 10.4;
+        // int num4 = (int) num3;
+        // System.out.println(num3);
+        // System.out.println(num4);
+
+        // /*
+        // * Print this pattern
+        // *
+        // * *
+        // * * *
+        // * * * *
+        // */
+        // System.out.println("*");
+        // System.out.println("**");
+        // System.out.println("***");
+        // System.out.println("****");
+
+        // // variables in java
+        // /*
+        // * datatypes are permitive and non permitive
+        // * premitive: byte(1), short(2), char(2), boolean(1), int(4), long(8),
+        // float(4),
+        // * double(8)
+        // * non permitive: String, array, class, object, interface
+        // */
+        // int a = 10;
+        // String name = "My name is Hafsa";
+        // double price = 10.99;
+        // double sum = a + price;
+        // System.out.println(name + " " + a + " " + price);
+        // System.out.println(sum);
+
+        // /*
+        // * Calculate
+        // * a*b/a-b
+        // * a=10, b=5
+        // * priority in java: * / % > - +
+        // */
+        // int up = 10 * 5;
+        // int dn = 10 - 5;
+        // double ans = up / dn;
+        // System.out.println("this is answer " + ans);
+
+        // // input in java
+        // Scanner sc = new Scanner(System.in);
+        // // next function only takes one token
+        // String nameee = sc.next();
+        // System.out.println(nameee);
+
+        // // next line takes an input of entire line
+        // Scanner nm = new Scanner(System.in);
+        // String nameeee = nm.nextLine();
+        // System.out.println(nameeee);
+
+        // // nextInt, nextFloat
+        // Scanner num = new Scanner(System.in);
+        // int value = num.nextInt();
+        // System.out.println(value);
+
+        // // Question: Take 2 varaibles 'g' and 'h' and print their sum
+        // Scanner scan = new Scanner(System.in);
+        // int g = scan.nextInt();
+        // int h = scan.nextInt();
+        // int sumof = g + h;
+        // System.out.println("The sum of g and h is: " + sumof);
+
+        
+    }
+}
